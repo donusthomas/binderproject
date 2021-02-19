f35c2369614143a25bece00a790bff9bcbb89378
donusu19 <donusthomas@live.com>
2021-02-18 20:32:03 -0400

    blog function

--- binder/settings.py
+++ binder/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,9 +120,15 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'binder/static/')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static' )
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media' )
